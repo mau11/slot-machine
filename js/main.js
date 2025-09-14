3e04519f0df77d366923b612cb59eb48eafdb822
Maureen E. Zitouni <maureen.zitouni@gmail.com>
2025-09-14 18:48:01 -0400

    Add confetti animation for wins

--- js/main.js
+++ js/main.js
@@ -40,11 +40,41 @@ function handleClick(bet) {
   spinReels(bet);
 }
 
+// Confetti source: https://www.kirilv.com/canvas-confetti/#continuous
+function win(color) {
+  // Continuous confetti for 5 seconds
+  let end = Date.now() + 5 * 1000;
+
+  let colors = [color, "#dededeff", "#333333ff"];
+
+  function frame() {
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+      colors: colors,
+    });
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+      colors: colors,
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  }
+  frame();
+}
+
 function spinReels(bet) {
   total -= bet;
 
   if (total === 0) {
-    total = "YOU LOSE";
+    results.innerText = "YOU LOSE";
   } else {
     first.src = `images/${options[getRandomIndex()]}`;
     second.src = `images/${options[getRandomIndex()]}`;
@@ -53,7 +83,27 @@ function spinReels(bet) {
     if (first.src === second.src && second.src === third.src) {
       smallBet.disabled = true;
       bigBet.disabled = true;
-      results.innerText = "YOU WON";
+
+      const team = first.src.split("/").pop().split(".")[0];
+
+      switch (team) {
+        case "patriots":
+          results.innerText = "PATRIOTS WIN";
+          win("#092142ff");
+          break;
+        case "bruins":
+          results.innerText = "BRUINS WIN";
+          win("#f4bb47ff");
+          break;
+        case "red-sox":
+          results.innerText = "RED SOX WIN";
+          win("#ae3c3eff");
+          break;
+        default:
+          results.innerText = "CELTICS WIN";
+          win("#39814eff");
+          break;
+      }
     }
   }
 
